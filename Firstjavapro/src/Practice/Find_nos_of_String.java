4cc71abbfe61284fda7c927abe791c16062c211b
comp <comp@DESKTOP-O562LT0>
2023-01-26 07:52:58 +0530

    Prasad-Find nos of String program added

--- Firstjavapro/src/Practice/Find_nos_of_String.java
+++ Firstjavapro/src/Practice/Find_nos_of_String.java
@@ -4,16 +4,16 @@ public class Find_nos_of_String {
 
 	public static void main(String[] args) {
 
-	String a="welcome";
-		
-		int x=a.length();
-		
-		int j=0;
-		for(int i=0; i<=x; i++)
-		{
-			j=i;
-		}
-		System.out.println(j);
+		String a = "welcome";
+
+//		int x = a.length();
+		System.out.println(a.length());
+//		int j = 0;
+//		for (int i = 0; i <= x; i++) 
+//		{
+//			j = i;
+//		}
+//		System.out.println(j);
 
 	}
 
